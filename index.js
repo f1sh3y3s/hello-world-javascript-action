15850e5186d5f3f32b932ec990277bbeed5e43bf
f1sh3y3s <45137859+f1sh3y3s@users.noreply.github.com>
2022-06-11 13:56:08 +0530

    collect all workflows

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const core = require("@actions/core")
-const github = require("@actions/github")
+const github = require("@actions/github");
 
 try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
+    const gitToken = core.getInput("github-token")
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
@@ -13,3 +14,25 @@ try {
 } catch (error) {
     core.setFailed(error.message);
 }
+
+async function run() {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        const gitToken = core.getInput("github-token")
+        console.log(`Hello ${nameToGreet}!`);
+        const octokit = github.getOctokit(gitToken)
+        const workflows = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', github.context.repo)
+        console.log(workflows)
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+
+}
+
+run()
