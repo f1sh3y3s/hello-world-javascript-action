86f9787d8f52588a217d28a6b1282e5f627454db
f1sh3y3s <45137859+f1sh3y3s@users.noreply.github.com>
2022-06-11 14:46:48 +0530

    'update

--- dist/index.js
+++ dist/index.js
@@ -8868,7 +8868,9 @@ async function run() {
         // `who-to-greet` input defined in action metadata file
         const nameToGreet = core.getInput('who-to-greet');
         console.log(`Hello ${nameToGreet}!`);
+        console.log(`token ${github.token} ${github.context.token} `)
         const octokit = github.getOctokit(github.token)
+
         const workflows = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', github.context.repo)
         console.log(workflows)
         const time = (new Date()).toTimeString();
