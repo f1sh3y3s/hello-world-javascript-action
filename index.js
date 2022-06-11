878f9a6beefa5ab56d53e4173f0edf1d50bb6d20
f1sh3y3s <45137859+f1sh3y3s@users.noreply.github.com>
2022-06-11 15:46:42 +0530

    add request workflows

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ async function run() {
         const nameToGreet = core.getInput('who-to-greet');
         const myToken = core.getInput("myToken")
         console.log(`Hello ${nameToGreet}!`);
-        console.log(`token ${myToken} ${github.token} `)
-        // const octokit = github.getOctokit(github.token)
 
-        // const workflows = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', github.context.repo)
-        // console.log(workflows)
+        const octokit = github.getOctokit(myToken)
+
+        const workflows = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', github.context.repo)
+        console.log(workflows)
         const time = (new Date()).toTimeString();
         core.setOutput("time", time);
         // Get the JSON webhook payload for the event that triggered the workflow
