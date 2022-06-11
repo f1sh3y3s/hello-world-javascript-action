52ea47325f321bb209191b0032d593a20870c5f8
f1sh3y3s <45137859+f1sh3y3s@users.noreply.github.com>
2022-06-11 13:56:08 +0530

    collect all workflows

--- dist/index.js
+++ dist/index.js
@@ -8861,11 +8861,12 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const core = __nccwpck_require__(4181)
-const github = __nccwpck_require__(5163)
+const github = __nccwpck_require__(5163);
 
 try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
+    const gitToken = core.getInput("github-token")
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
@@ -8875,6 +8876,28 @@ try {
 } catch (error) {
     core.setFailed(error.message);
 }
+
+async function run() {
+    try {
+        // `who-to-greet` input defined in action metadata file
+        const nameToGreet = core.getInput('who-to-greet');
+        const gitToken = core.getInput("github-token")
+        console.log(`Hello ${nameToGreet}!`);
+        const octokit = github.getOctokit(gitToken)
+        const workflows = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', github.context.repo)
+        console.log(workflows)
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+
+}
+
+run()
 })();
 
 module.exports = __webpack_exports__;
