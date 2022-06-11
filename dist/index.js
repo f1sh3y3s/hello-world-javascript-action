558fa7d80af685b7eeef336acd561ad3b532a66e
f1sh3y3s <45137859+f1sh3y3s@users.noreply.github.com>
2022-06-11 14:23:07 +0530

    use git hub token inside

--- dist/index.js
+++ dist/index.js
@@ -8863,27 +8863,12 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(4181)
 const github = __nccwpck_require__(5163);
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    const gitToken = core.getInput("github-token")
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
-
 async function run() {
     try {
         // `who-to-greet` input defined in action metadata file
         const nameToGreet = core.getInput('who-to-greet');
-        const gitToken = core.getInput("github-token")
         console.log(`Hello ${nameToGreet}!`);
-        const octokit = github.getOctokit(gitToken)
+        const octokit = github.getOctokit(github.token)
         const workflows = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', github.context.repo)
         console.log(workflows)
         const time = (new Date()).toTimeString();
--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 const core = require("@actions/core")
 const github = require("@actions/github");
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    const gitToken = core.getInput("github-token")
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
-
 async function run() {
     try {
         // `who-to-greet` input defined in action metadata file
         const nameToGreet = core.getInput('who-to-greet');
-        const gitToken = core.getInput("github-token")
         console.log(`Hello ${nameToGreet}!`);
-        const octokit = github.getOctokit(gitToken)
+        const octokit = github.getOctokit(github.token)
         const workflows = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', github.context.repo)
         console.log(workflows)
         const time = (new Date()).toTimeString();
